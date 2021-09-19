88732e2e07ee0d12143129dcf23774b0ed4eec28
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-20 02:43:17 +0530

    Add:-Welcome message

---
+++ src/com/Bridgelabs/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.Bridgelabs;
+
+public class EmployeeWageComputation {
+	public static void main(String arsg[]) {
+		System.out.println("**Welcome to the employee wage computation program**");
+	}
+
+}
