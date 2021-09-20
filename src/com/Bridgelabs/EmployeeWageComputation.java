9aaeb08503ba291045da33fb6c20a6d397a37484
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-20 08:08:59 +0530

    Add:-Employee  daily wage calculation

--- src/com/Bridgelabs/EmployeeWageComputation.java
+++ src/com/Bridgelabs/EmployeeWageComputation.java
@@ -5,13 +5,20 @@ public class EmployeeWageComputation {
 		System.out.println("**Welcome to the employee wage computation program**");
 		
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOURS =20;
+		int empHrs = 0;
+        int empWage = 0;
+        
 		double empCheck =Math.floor(Math.random() * 10) % 2;
 		
 		
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("**Employee is Present**");
+			empHrs=8;
 		else
-			System.out.println("**Employee is Absent**");
+			empHrs=0;
+		
+		empWage = empHrs * EMP_RATE_PER_HOURS;
+		System.out.println("Employee Wage: " + empWage);
 	}
 
 }
