6dfd66ac0f00a741151f1560b231e27074ac2938
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-24 08:32:05 +0530

    Add:-Compute employee wage using class methods

--- src/com/Bridgelabs/EmployeeWageComputation.java
+++ src/com/Bridgelabs/EmployeeWageComputation.java
@@ -2,44 +2,54 @@ package com.Bridgelabs;
 
 public class EmployeeWageComputation {
 	
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
-	public static final int EMP_RATE_PER_HOURS =20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	 public static final int MAX_HRS_IN_MONTH = 100;
+		public static final int IS_FULL_TIME = 1;
+		public static final int IS_PART_TIME = 2;
+		public static final int EMPLOYEE_RATE_PER_HOURS =20;
+		public static final int NUMBER_OF_WORKING_DAYS = 20;
+		public static final int MAXIMUM_HRS_IN_MONTH = 100;
 	
-	public static void main(String arsg[]) {
-		System.out.println("**Welcome to the employee wage computation program**");
 		
+		public static void main(String arsg[]) {
+			System.out.println("**Welcome to the employee wage computation program**");
+			computeEmployeeWage();
+		}
 		
-		int empHrs = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
+		//creating method
+		public static int computeEmployeeWage() {
+			
+			//local Variable
+			int employeeHours = 0;
+			int totalEmployeeHours = 0;
+			int totalWorkingDays = 0;
         
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-            totalWorkingDays++;
+			//using while loop for iteration
+			while (totalEmployeeHours <= MAXIMUM_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+				totalWorkingDays++;
             
     
-		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-		
-			switch  (empCheck) {
-			case IS_FULL_TIME:
-				empHrs=8;
-				break;
-			case IS_PART_TIME:
-				empHrs =4;
-				break;
-			default:
-				empHrs=0;
-			}
+				int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
+				
+				//using switch case
+				switch  (employeeCheck) {
+					case IS_FULL_TIME:
+						employeeHours=8;
+						break;
+					case IS_PART_TIME:
+						employeeHours =4;
+						break;
+					default:
+						employeeHours=0;
+				}
 			
 		
-		totalEmpHrs += empHrs;
-		System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
+				totalEmployeeHours += employeeHours;
+				System.out.println("Day#: " + totalWorkingDays + " Employee Hour: " +employeeHours);
 		
-        }
+			}
         
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOURS;
-        System.out.println("Total Emp Wage: " + totalEmpWage);
-	}
+			int totalEmployeeWage = totalEmployeeHours * EMPLOYEE_RATE_PER_HOURS;
+			System.out.println("Total Employee Wage: " + totalEmployeeWage);
+			return totalEmployeeWage;
+		}
+		
 }
