3532ed3ae26bb3c4952cff898d4686db98dbb0c2
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-26 11:54:49 +0530

    Add:-Create employee wage builder for each company

--- src/com/Bridgelabs/EmployeeWageComputation.java
+++ src/com/Bridgelabs/EmployeeWageComputation.java
@@ -5,49 +5,65 @@ public class EmployeeWageComputation {
 		public static final int IS_FULL_TIME = 1;
 		public static final int IS_PART_TIME = 2;
 	
+		private final String companyName;
+		private final int employeeRatePerHours;
+		private final int numberOfWorkingDays;
+		private final int maximumHoursPerMonth;
+		private int totalEmployeeWage;
 		
-		public static void main(String arsg[]) {
-			System.out.println("**Welcome to the employee wage computation program**");
-			computeEmployeeWage("SoftwareOne", 410, 20, 100);
-			computeEmployeeWage("Infosys", 860, 20, 120);
+		
+		//creating constructor
+		public EmployeeWageComputation(String companyName, int employeeRatePerHours, int numberOfWorkingDays, int maximumHoursPerMonth) {
+			
+			this.companyName = companyName;
+			this.employeeRatePerHours = employeeRatePerHours;
+			this.numberOfWorkingDays = numberOfWorkingDays;
+			this.maximumHoursPerMonth = maximumHoursPerMonth;
 		}
 		
-		//creating method
-		public static int computeEmployeeWage(String companyName, int employeeRatePerHours, int numberOfWorkingDays, int maximumHoursPerMonth) {
+		public void computeEmployeeWage() {
 			
-			//local Variable
-			int employeeHours = 0;
-			int totalEmployeeHours = 0;
-			int totalWorkingDays = 0;
-        
+			int employeeHours =0, totalEmployeeHours =0, totalWorkingDays = 0;	
+			
 			//using while loop for iteration
-			while (totalEmployeeHours <= maximumHoursPerMonth && totalWorkingDays < numberOfWorkingDays) {
-				totalWorkingDays++;
-            
-    
-				int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
+				while (totalEmployeeHours <= maximumHoursPerMonth && totalWorkingDays < numberOfWorkingDays) {
+					totalWorkingDays++;
+					int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
 				
 				//using switch case
-				switch  (employeeCheck) {
-					case IS_FULL_TIME:
-						employeeHours=8;
-						break;
-					case IS_PART_TIME:
-						employeeHours =4;
-						break;
-					default:
-						employeeHours=0;
-				}
+					switch  (employeeCheck) {
+						case IS_FULL_TIME:
+							employeeHours=8;
+							break;
+						case IS_PART_TIME:
+							employeeHours =4;
+							break;
+						default:
+							employeeHours=0;
+					}
 			
 		
 				totalEmployeeHours += employeeHours;
-				System.out.println("Day#: " + totalWorkingDays + " Employee Hour: " +employeeHours);
+				System.out.println("Day#: " + totalWorkingDays + " Employee Daily Working Hour: " +employeeHours);
 		
-			}
-        
-			int totalEmployeeWage = totalEmployeeHours * employeeRatePerHours ;
-			System.out.println("Total Employee Wage for company " + companyName+ " is:" + totalEmployeeWage);
-			return totalEmployeeWage;
+				}
+				totalEmployeeWage = totalEmployeeHours * employeeRatePerHours;
 		}
 		
+			public String toString(){
+			return "Total employee wage for company" +companyName+ " is:" + totalEmployeeWage;
+			}
+			
+			public static void main(String arsg[]) {
+			
+			System.out.println("**********************************************************\nWelcome to the employee wage computation program\n**********************************************************");
+			EmployeeWageComputation softwareOne = new EmployeeWageComputation("SoftwareOne",1100,10,100);
+			EmployeeWageComputation infosys = new EmployeeWageComputation("Infosys",1200,10,100);
+			
+			softwareOne.computeEmployeeWage();
+			System.out.println("*********************************************************\n"+softwareOne+"\n*********************************************************");
+			infosys.computeEmployeeWage();
+			System.out.println("***********************************************************\n"+infosys+"\n***********************************************************");
+			
+			}	
 }
